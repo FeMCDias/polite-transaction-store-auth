ad82410d3433db293f8c1aed97349ea06120dd0e
FeMCDias <femcdias@gmail.com>
2024-03-20 08:05:01 -0300

    feat: Adding Solve

---
+++ src/main/java/politetransaction/store/auth/SolveIn.java
@@ -0,0 +1,9 @@
+package politetransaction.store.auth;
+import lombok.Builder;
+
+@Builder
+public record SolveIn(
+    String token
+) {
+    
+}
---
+++ src/main/java/politetransaction/store/auth/SolveOut.java
@@ -0,0 +1,10 @@
+package politetransaction.store.auth;
+import lombok.Builder;
+
+@Builder
+public record SolveOut(
+    String id,
+    String name,
+    String role
+) {
+}
